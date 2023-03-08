604a91aa49a8ced6fdf9a4e527186f2cdbad0165
TC008800 <Kevin3617@outlook.com>
2023-03-08 22:12:12 +0800

    [add] gradient OK!

--- navigation/pcl2process/src/pcl2process.cpp
+++ navigation/pcl2process/src/pcl2process.cpp
@@ -14,21 +14,44 @@
 
 ros::Publisher pcl_publisher;
 
-void getcloud(const sensor_msgs::PointCloud2ConstPtr& laserCloudMsg){
-    pcl::PointCloud<pcl::PointXYZ>::Ptr pcl2cloud(new pcl::PointCloud<pcl::PointXYZ>);
+struct PointInt {
+    int p_x, p_y, p_z;
+};
+
+void getcloud(const sensor_msgs::PointCloud2ConstPtr &laserCloudMsg) {
+    std::vector <std::vector<int>> point_list1(1200);
+    std::vector <PointInt> point_list2;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pcl2cloud(new pcl::PointCloud <pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pcl2cloud_out(new pcl::PointCloud <pcl::PointXYZ>);
     pcl::fromROSMsg(*laserCloudMsg, *pcl2cloud);
-    cv::Mat hight_map(1200,1200,CV_8UC1,cv::Scalar(0));
-    for(auto point : (pcl2cloud->points) ){
-        if(point.x < 5.8){
-            if(point.x > -5.8){
-                if(point.y < 5.8){
-                    if(point.y > -5.8){
-                        if(point.z > -0.4){
-                            if (point.z < 2){
+    cv::Mat hight_map(1201, 1201, CV_8UC1, cv::Scalar(0));
+    cv::Mat gradient_map(1201, 1201, CV_8UC1, cv::Scalar(0));
+    for (auto point: (pcl2cloud->points)) {
+        if (point.x < 5.8) {
+            if (point.x > -5.8) {
+                if (point.y < 5.8) {
+                    if (point.y > -5.8) {
+                        if (point.z > -1.2) {
+                            if (point.z < 1.2) {
+                                if (point.x < 0.4 and point.x > -0.4) {
+                                    if (point.y < 0.4 and point.y > -0.4) {
+                                        continue;
+                                    }
+                                }
                                 int px = 100 * point.x + 600;
                                 int py = 100 * point.y + 600;
-                                unsigned char pz= 100 * point.z + 45;
-                                hight_map.at<uchar>(px,py)=pz;
+                                unsigned char pz = 100 * point.z + 130;
+                                if (!(hight_map.at<uchar>(px, py))) {
+                                    hight_map.at<uchar>(px, py) = pz;
+                                    point_list1[px].push_back(py);
+                                    point_list2.push_back({px, py, pz});
+                                } else {
+                                    if (pz < hight_map.at<uchar>(px, py)) {
+                                        hight_map.at<uchar>(px, py) = pz;
+                                        point_list1[px].push_back(py);
+                                        point_list2.push_back({px, py, pz});
+                                    }
+                                }
                             }
                         }
                     }
@@ -36,7 +59,29 @@ void getcloud(const sensor_msgs::PointCloud2ConstPtr& laserCloudMsg){
             }
         }
     }
-    cv::imshow("hight_map", hight_map);
+    for (auto point: point_list2) {
+        unsigned char max = 0;
+        unsigned char min = 255;
+        for (int surround_point_x = (((point.p_x - 10) > 0) ? (point.p_x - 10) : 0);
+                surround_point_x < (((point.p_x + 10) < 1199) ? (point.p_x + 10) : 1199);
+                surround_point_x = surround_point_x + 1) {
+            for (auto surround_point_y : point_list1[surround_point_x]) {
+                if((surround_point_y < point.p_y + 10) and (surround_point_y > point.p_y - 10)){
+                    if(hight_map.at<uchar>(surround_point_x, surround_point_y) < min){
+                        min = hight_map.at<uchar>(surround_point_x, surround_point_y);
+                    }
+                    if(hight_map.at<uchar>(surround_point_x, surround_point_y) > max){
+                        max = hight_map.at<uchar>(surround_point_x, surround_point_y);
+                    }
+                }
+                if(surround_point_y > point.p_y + 10){
+                    break;
+                }
+            }
+        }
+        gradient_map.at<uchar>(point.p_x, point.p_y) = max - min;
+    }
+    cv::imshow("hight_map", gradient_map);
     cv::waitKey(1);
     //ROS_INFO("%d",pcl2cloud->points.size());
 }
