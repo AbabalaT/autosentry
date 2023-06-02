61552f082d0139d91d628099e37fee9e2e3b0d83
TC008800 <Kevin3617@outlook.com>
2023-06-02 10:21:48 +0800

    [update] emergent update 04

--- logic_control/PE.py
+++ logic_control/PE.py
@@ -44,16 +44,16 @@ yaw_scan_low = 0.0
 yaw_scan_up = 0.0
 yaw_scan_state = 0  # 0：周扫描 1：扇正扫 2：扇反扫
 
-target_publish_idle = 10
+target_publish_idle = 0
+yaw_refresh_idle = 0
 
-remain_time = 300
+remain_time = 420
 require_add_HP = 0
-self_robot_HP = 600
+
+self_robot_HP = 1000
 self_outpost_HP = 1500
-
-pre_base_HP = 1500
-
 self_base_HP = 5000
+pre_base_HP = 5000
 
 enemy_robot_HP = 600
 enemy_outpost_HP = 1500
@@ -61,12 +61,8 @@ enemy_base_HP = 5000
 
 self_color = 'blue'
 
-save_base_time_cnt = 0  # 单位0.1秒
-wait_attack_cnt = 0
-attack_cnt = 0  # 攻击模式计时器
 hurt_lock_pos_cnt = 0
 aim_lock_pos_cnt = 0
-follow_enemy_cnt = 0
 
 enemy_1_cnt = 0
 enemy_2_cnt = 0
@@ -74,7 +70,8 @@ enemy_3_cnt = 0
 enemy_4_cnt = 0
 enemy_5_cnt = 0
 
-gimbal_control_cnt = 0
+moving_cnt = -1
+moving_direction = 0
 
 strategy_state = 0  # 0： 防御模式 1： 攻击模式 2： 视觉拖曳
 
@@ -107,7 +104,7 @@ current_x = 0.0
 current_y = 0.0
 current_yaw = 0.0
 
-aim_switch = [0, 1, 1, 1, 1, 1, 1, 1, 1]
+aim_switch = [0, 1, 0, 1, 1, 1, 0, 0, 0]
 
 '''
 0:待机、 1：攻击、 2：防御
@@ -119,8 +116,6 @@ aim_switch = [0, 1, 1, 1, 1, 1, 1, 1, 1]
     路上发现目标会攻击但不改变运动策略
     到达后旋转扫描
 '''
-VIO_switch = 0
-LIO_switch = 1
 
 pitch_scan = 0.0
 pitch_state = 0  # 0:上升 1: 下降
@@ -148,116 +143,36 @@ rot = (0.0, 0.0, 0.0, 0.0)
 def armor_select_callback(event):
     global armor_select_publisher, aim_switch
     aim_select_msg = armor_select()
-    if strategy_state == 0:
-        aim_select_msg.aim_1 = 255
-        if kill_enemy_engineer:
-            aim_select_msg.aim_2 = 255
-        else:
-            aim_select_msg.aim_2 = 0
-        aim_select_msg.aim_3 = 255
-        aim_select_msg.aim_4 = 255
-        aim_select_msg.aim_5 = 255
-        aim_select_msg.aim_robot = 0
-        aim_select_msg.aim_base = 0
-        aim_select_msg.aim_outpost = 0
-    elif strategy_state == 1:
-        aim_select_msg.aim_1 = 0
-        aim_select_msg.aim_2 = 0
-        aim_select_msg.aim_3 = 0
-        aim_select_msg.aim_4 = 0
-        aim_select_msg.aim_5 = 0
-        if enemy_outpost_HP > 0:
-            aim_select_msg.aim_outpost = 1
-            aim_select_msg.aim_robot = 0
-            aim_select_msg.aim_base = 0
-        elif enemy_robot_HP > 0 and enemy_base_HP == 5000:
-            aim_select_msg.aim_outpost = 0
-            aim_select_msg.aim_robot = 1
-            aim_select_msg.aim_base = 0
-        elif enemy_robot_HP > 0 and enemy_base_HP != 5000:
-            if kill_sentry_first:
-                aim_select_msg.aim_outpost = 0
-                aim_select_msg.aim_robot = 1
-                aim_select_msg.aim_base = 0
-            else:
-                aim_select_msg.aim_outpost = 0
-                aim_select_msg.aim_robot = 0
-                aim_select_msg.aim_base = 1
-        else:
-            aim_select_msg.aim_outpost = 0
-            aim_select_msg.aim_robot = 0
-            aim_select_msg.aim_base = 1
-    elif strategy_state == 2:
-        aim_select_msg.aim_1 = 0
-        aim_select_msg.aim_2 = 0
-        aim_select_msg.aim_3 = 0
-        aim_select_msg.aim_4 = 0
-        aim_select_msg.aim_5 = 0
-        aim_select_msg.aim_robot = 0
-        aim_select_msg.aim_base = 0
-        aim_select_msg.aim_outpost = 0
-    if invincible_detect:
-        if enemy_1_cnt > 1:
-            aim_select_msg.aim_1 = 0
-        if enemy_2_cnt > 1:
-            aim_select_msg.aim_2 = 0
-        if enemy_3_cnt > 1:
-            aim_select_msg.aim_3 = 0
-        if enemy_4_cnt > 1:
-            aim_select_msg.aim_4 = 0
-        if enemy_5_cnt > 1:
-            aim_select_msg.aim_5 = 0
-    aim_switch[1] = aim_select_msg.aim_1
-    aim_switch[2] = aim_select_msg.aim_2
-    aim_switch[3] = aim_select_msg.aim_3
-    aim_switch[4] = aim_select_msg.aim_4
-    aim_switch[5] = aim_select_msg.aim_5
-    aim_switch[6] = aim_select_msg.aim_robot
-    aim_switch[7] = aim_select_msg.aim_outpost
-    aim_switch[8] = aim_select_msg.aim_base
-
+    aim_select_msg.aim_1 = aim_switch[1]
+    aim_select_msg.aim_2 = aim_switch[2]
+    aim_select_msg.aim_3 = aim_switch[3]
+    aim_select_msg.aim_4 = aim_switch[4]
+    aim_select_msg.aim_5 = aim_switch[5]
+    aim_select_msg.aim_robot = aim_switch[6]
+    aim_select_msg.aim_outpost = aim_switch[7]
+    aim_select_msg.aim_base = aim_switch[8]
     armor_select_publisher.publish(aim_select_msg)
 
 
 def strategy_callback(event):
     global warning_cnt, strategy_target_yaw, yaw_scan_state, command_cnt
     global strategy_target_x, strategy_target_y, strategy_state
-
-    if warning_cnt > 0:
-        warning_cnt = warning_cnt - 1
-        vector_x = enemy_location_x - current_x
-        vector_y = enemy_location_y - current_y
-        vector_theta = math.atan(vector_y / vector_x)
-        if vector_x < 0:
-            if vector_y > 0:
-                vector_theta = vector_theta + 3.1415926
-            else:
-                vector_theta = vector_theta - 3.1415926
-        strategy_target_yaw = vector_theta
-    if warning_cnt < 0:
-        warning_cnt = 0
-    if warning_cnt == 0:
-        yaw_scan_state = 0
-
     if command_cnt > 0:
-        command_cnt = command_cnt - 1
         strategy_target_x = commander_x
         strategy_target_y = commander_y
     elif game_status != 4:
-        strategy_state = 0
         strategy_target_x = 0.0
         strategy_target_y = 0.0
     elif self_outpost_HP < 10:
-        strategy_state = 0
         strategy_target_x = 0.0 + random.uniform(-0.5, 0.5)
         strategy_target_y = 0.0 + random.uniform(-0.5, 0.5)
+    print('        strategy_target:', strategy_target_x, strategy_target_y)
 
 
 def auto_aim_callback(ext_aim):
     global aim_lock_pos_cnt, aim_distance
     if aim_switch[ext_aim.target_number] != 0:
         aim_lock_pos_cnt = 0.8
-        # aim_distance = ext_aim.
 
 
 def game_status_callback(ext_status):
@@ -275,6 +190,7 @@ def game_HP_callback(ext_HP):
         self_robot_HP = ext_HP.red_7_robot_HP
         self_base_HP = ext_HP.red_base_HP
         self_outpost_HP = ext_HP.red_outpost_HP
+
         enemy_robot_HP = ext_HP.blue_7_robot_HP
         enemy_base_HP = ext_HP.blue_base_HP
         enemy_outpost_HP = ext_HP.blue_outpost_HP
@@ -293,6 +209,7 @@ def game_HP_callback(ext_HP):
         self_robot_HP = ext_HP.blue_7_robot_HP
         self_base_HP = ext_HP.blue_base_HP
         self_outpost_HP = ext_HP.blue_outpost_HP
+
         enemy_robot_HP = ext_HP.red_7_robot_HP
         enemy_base_HP = ext_HP.red_base_HP
         enemy_outpost_HP = ext_HP.red_outpost_HP
@@ -307,7 +224,7 @@ def game_HP_callback(ext_HP):
             enemy_4_cnt = 10
         if ext_HP.red_5_robot_HP == 0:
             enemy_5_cnt = 10
-    # print("                               HP:", robot_HP)
+    # print("    HP:", robot_HP)
 
 
 def chassis_angle_callback(ext_chassis_angle):
@@ -338,7 +255,6 @@ def cnt_timer_callback(event):
 
 def death_robot_callback(event):
     global enemy_1_cnt, enemy_2_cnt, enemy_3_cnt, enemy_4_cnt, enemy_5_cnt
-
     if enemy_1_cnt > 0:
         enemy_1_cnt = enemy_1_cnt - 1
     else:
@@ -363,7 +279,7 @@ def death_robot_callback(event):
 
 def target_xyz_callback():
     global self_base_HP, pre_base_HP, wait_attack_cnt, aim_lock_pos_cnt, target_yaw, current_yaw, target_x, target_y
-    global pre_target_x, pre_target_y, pre_target_yaw, yaw_scan_state, target_publish_idle
+    global pre_target_x, pre_target_y, pre_target_yaw, yaw_scan_state, target_publish_idle, yaw_refresh_idle
     target_pose = PoseStamped()
     target_pose.header.frame_id = "map"
     frame_target_yaw = 0.0
@@ -380,21 +296,19 @@ def target_xyz_callback():
     else:
         target_pose.pose.position.x = strategy_target_x
         target_pose.pose.position.y = strategy_target_y
-        if abs(target_yaw - current_yaw) <= 0.55:  # 0.349
-            if yaw_scan_state == 0:
+        if yaw_refresh_idle > 0:
+            yaw_refresh_idle = yaw_refresh_idle - 1
+            if abs(target_yaw - current_yaw) <= 0.35:  # 0.349
                 target_yaw = target_yaw + 1.571
-            elif yaw_scan_state == 1:
-                target_yaw = strategy_target_yaw - 0.7
-                yaw_scan_state = 2
-            else:
-                target_yaw = strategy_target_yaw + 0.7
-                yaw_scan_state = 1
-
-            if target_yaw > 3.14159:
-                target_yaw = target_yaw - 6.2831852
-            if target_yaw < -3.14159:
-                target_yaw = target_yaw + 6.2881852
-        frame_target_yaw = target_yaw
+                yaw_refresh_idle = 10
+        else:
+            target_yaw = target_yaw + 1.571
+            yaw_refresh_idle = 10
+        if target_yaw > 3.14159:
+            target_yaw = target_yaw - 6.2831852
+        if target_yaw < -3.14159:
+            target_yaw = target_yaw + 6.2881852
+    frame_target_yaw = target_yaw
 
     # 发布
     if target_publish_idle > 0:
@@ -423,56 +337,77 @@ def target_xyz_timer_callback(event):
 
 def game_command_callback(ext_command):
     global command_cnt, target_x, target_y, enemy_location_x, enemy_location_y, warning_cnt
-    global strategy_state, yaw_scan_state, commander_x, commander_y
+    global strategy_state, yaw_scan_state, commander_x, commander_y, aim_switch
     global force_spinning, kill_sentry_first, kill_enemy_engineer, invincible_detect
+    global moving_cnt, moving_direction
+    if self_color == 'blue':
+        commander_x = 22.1 - ext_command.target_position_x
+        commander_y = 8.0 - ext_command.target_position_y
+    else:
+        commander_x = ext_command.target_position_x - 5.8
+        commander_y = ext_command.target_position_y - 8.0
+
     if ext_command.command_keyboard == 65:
-        strategy_state = 1
-        yaw_scan_state = 0
+        aim_switch = [0, 1, 0, 1, 1, 1, 0, 0, 0]
         command_cnt = 30
-        command_cnt = 30
-        if self_color == 'blue':
-            commander_x = 22.1 - ext_command.target_position_x
-            commander_y = 8.0 - ext_command.target_position_y
-        else:
-            commander_x = ext_command.target_position_x - 5.8
-            commander_y = ext_command.target_position_y - 8.0
     elif ext_command.command_keyboard == 66:
-        yaw_scan_state = 0
-        strategy_state = 0
+        aim_switch = [0, 0, 0, 0, 0, 0, 0, 1, 0]
         command_cnt = 30
-        if self_color == 'blue':
-            commander_x = 22.1 - ext_command.target_position_x
-            commander_y = 8.0 - ext_command.target_position_y
-        else:
-            commander_x = ext_command.target_position_x - 5.8
-            commander_y = ext_command.target_position_y - 8.0
-    elif ext_command.command_keyboard == 73:
-        warning_cnt = 10
-        yaw_scan_state = 1
-        if self_color == 'blue':
-            enemy_location_x = 22.1 - ext_command.target_position_x
-            enemy_location_y = 8.0 - ext_command.target_position_y
-        else:
-            enemy_location_x = ext_command.target_position_x - 5.8
-            enemy_location_y = ext_command.target_position_y - 8.0
     elif ext_command.command_keyboard == 67:
         command_cnt = 30
-        strategy_state = 2
-    elif ext_command.command_keyboard == 72:
-        force_spinning = True
-    elif ext_command.command_keyboard == 70:
-        kill_enemy_engineer = True
-    elif ext_command.command_keyboard == 71:
-        kill_enemy_engineer = False
-    elif ext_command.command_keyboard == 76:
-        kill_sentry_first = False
-    elif ext_command.command_keyboard == 78:
-        kill_sentry_first = True
+        aim_switch = [0, 1, 1, 1, 1, 1, 0, 0, 0]
     elif ext_command.command_keyboard == 68:
-        invincible_detect = True
+        command_cnt = 30
+        aim_switch = [0, 0, 0, 0, 0, 0, 1, 0, 0]
     elif ext_command.command_keyboard == 69:
-        invincible_detect = False
+        command_cnt = 30
+        aim_switch = [0, 0, 0, 0, 0, 0, 0, 0, 1]
+
+    elif ext_command.command_keyboard == 84:
+        force_spinning = True
+    
+    elif ext_command.command_keyboard == 73:
+        moving_cnt = 3.2
+        moving_direction = 0
+    elif ext_command.command_keyboard == 74:
+        moving_cnt = 3.2
+        moving_direction = 1
+    elif ext_command.command_keyboard == 75:
+        moving_cnt = 3.2
+        moving_direction = 2
+    elif ext_command.command_keyboard == 76:
+        moving_cnt = 3.2
+        moving_direction = 3
+    
     target_xyz_callback()
+
+def force_moving_callback(event):
+    global moving_cnt, moving_direction
+    if moving_cnt >= 0:
+        moving_cnt = moving_cnt - 0.1
+        force_speed = Twist()
+        if moving_cnt > 0.2:
+            if moving_direction == 0:
+                force_speed.linear.x = 0.33
+                force_speed.linear.y = 0
+                cmd.angular.z = 0
+            elif moving_direction == 1:
+                force_speed.linear.x = 0
+                force_speed.linear.y = 0.33
+                cmd.angular.z = 0
+            elif moving_direction == 2:
+                force_speed.linear.x = -0.33
+                force_speed.linear.y = 0
+                cmd.angular.z = 0
+            elif moving_direction == 3:
+                force_speed.linear.x = 0
+                force_speed.linear.y = -0.33
+                cmd.angular.z = 0
+        else:
+            force_speed.linear.x = 0
+            force_speed.linear.y = 0
+            cmd.angular.z = 0
+        force_moving_publisher.publish(force_speed)
 
 
 def spin_timer_callback(event):
@@ -494,16 +429,17 @@ def spin_timer_callback(event):
         spin_speed_msg.spinning_speed = target_spinning_speed + random.randint(0, 4000)
     # spin_speed_msg.spinning_speed = 0
     spin_speed_publisher.publish(spin_speed_msg)
-    print('     Debug:', strategy_target_x, strategy_target_y)
+    print('     Debug:', strategy_target_x, strategy_target_y, 'lowest spinning:', lowest_spinning_speed)
 
 
 def game_hurt_callback(ext_hurt):
     global target_spinning_speed, hurt_angle, armor0_angle, hurt_lock_pos_cnt
     if hurt_lock_pos_cnt < 1.0:
-        if ext_hurt.hurt_type == 0 and lowest_spinning_speed != 0:
-            target_spinning_speed = 26000
+        if ext_hurt.hurt_type == 0:
             hurt_angle = armor0_angle + ext_hurt.armor_id * 1.571
             hurt_lock_pos_cnt = 1.5
+            if lowest_spinning_speed != 0:
+                target_spinning_speed = 26000
             target_xyz_callback()
 
 
@@ -520,27 +456,25 @@ def tf_get_timer_callback(event):
 
 
 def pitch_timer_callback(event):
-    global pitch_state, pitch_scan, pitch_scan_low, pitch_scan_up, enemy_outpost_HP, strategy_state
-    if enemy_outpost_HP > 0 and strategy_state == 1:
+    global pitch_state, pitch_scan, pitch_scan_low, pitch_scan_up, aim_switch
+    if aim_switch[7] > 0:
         pitch_scan_up = 25
         pitch_scan_low = 0
     else:
         pitch_scan_low = -20.0
-        pitch_scan_up = 20
+        pitch_scan_up = 15
 
-    if aim_lock_pos_cnt >= 0:
+    if aim_lock_pos_cnt > 0:
         pass
     else:
         if pitch_state == 0:
-            pitch_scan = pitch_scan + 6
+            pitch_scan = pitch_scan + 2
             if pitch_scan > pitch_scan_up:
                 pitch_state = 1
-        elif pitch_state == 1:
-            pitch_scan = pitch_scan - 6
+        else:
+            pitch_scan = pitch_scan - 2
             if pitch_scan < pitch_scan_low:
                 pitch_state = 0
-        else:
-            pass
     recommend_pitch_publisher.publish(pitch_scan)
 
 
@@ -551,6 +485,7 @@ if __name__ == '__main__':
     recommend_pitch_publisher = rospy.Publisher('/robot/logic_recommend_angle', Float32, queue_size=1)
     spin_speed_publisher = rospy.Publisher('/robot/spinning_speed', spinning_control, queue_size=1)
     armor_select_publisher = rospy.Publisher('/robot/armor_select', armor_select, queue_size=1)
+    force_moving_publisher = rospy.Publisher('/cmd_vel', Twist)
 
     chassis_angle_sub = rospy.Subscriber('/robot/chassis_angle', Float32, chassis_angle_callback)
     referee_status_sub = rospy.Subscriber('/referee/status', message_game_status, game_status_callback)
@@ -567,6 +502,7 @@ if __name__ == '__main__':
     timer_06 = rospy.Timer(rospy.Duration(0.1), cnt_timer_callback)
     timer_07 = rospy.Timer(rospy.Duration(1), death_robot_callback)
     timer_08 = rospy.Timer(rospy.Duration(0.5), armor_select_callback)
+    timer_09 = rospy.Timer(rospy.Duration(0.1), force_moving_callback)
     rospy.spin()
 
     recommend_pitch_publisher.publish(0)
